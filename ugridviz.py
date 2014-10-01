577a6cc3d3c943dea808bbd234a741d175646ef8
Chris Calloway <cbc@chriscalloway.org>
2014-10-01 19:38:04 -0400

    Documentation refresh.

--- ugridviz.py
+++ ugridviz.py
@@ -12,18 +12,29 @@ from PyQt4 import QtGui, QtCore
 
 
 class UgridViz(QtGui.QWidget):
-
+    """
+    The main display widget.
+    """
     def __init__(self):
+        """
+        Override parent's special init method.
+        """
         super(UgridViz, self).__init__()
         self.initGui()
 
     def initGui(self):
+        """
+        Consolidate initialization of the widget.
+        """
         self.dimension()
         self.decorate()
         self.show()
         return
 
     def dimension(self):
+        """
+        Dimension and position the widget.
+        """
         ag = QtGui.QDesktopWidget().availableGeometry()
         self.resize(3 * ag.width() / 4, 3 * ag.height() / 4)
         fg = self.frameGeometry()
@@ -32,15 +43,24 @@ class UgridViz(QtGui.QWidget):
         return
 
     def decorate(self):
+        """
+        Decorate the window with the usual graphic elements.
+        """
         self.setWindowTitle('UgridViz')
         return
 
     def closeEvent(self, event):
+        """
+        Intercept exiting the application
+        """
         event.accept()
         return
 
 
 def main():
+    """
+    Consolidate running the application.
+    """
     QtCore.pyqtRemoveInputHook()
     app = QtGui.QApplication(sys.argv)
     ugridviz = UgridViz()
